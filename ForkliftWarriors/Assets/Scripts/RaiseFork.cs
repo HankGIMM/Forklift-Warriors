273399e5d755892cb293ddf47e01ceab70f60e93
StevenDrain <stevendrain@u.boisestate.edu>
2026-04-13 17:57:41 -0600

    Squashed commit of the following:
    commit a140fff58fb2e694fb0b06669a8f251a4c725aac
    Author: StevenDrain <stevendrain@u.boisestate.edu>
    Date:   Mon Apr 13 17:55:18 2026 -0600
    
        fixed left right lever
    
        now actually moves left and right rather than forward/backward
    
    commit 9cd14927063d58c58c402ce3650dfe2fd2e40f7f
    Author: StevenDrain <stevendrain@u.boisestate.edu>
    Date:   Mon Apr 13 17:20:14 2026 -0600
    
        fixed levers and dumb mirrored camera bug
    
    commit 3daf84662232bb834fdecee6c73ed3bf216998fe
    Author: StevenDrain <stevendrain@u.boisestate.edu>
    Date:   Sat Apr 11 21:23:26 2026 -0600
    
        Forklift Changes
    
        I changed the whole lever script to one I found online and edited slightly and added the forklift model.
    
    commit ffb3769c3b0208826e52c5263b70773e081575b8
    Author: StevenDrain <stevendrain@u.boisestate.edu>
    Date:   Thu Apr 9 21:55:49 2026 -0600
    
        small changes that i dont remember making
    
    commit 1fae57ffbd899b5f71a5050af7800bd59c5976f1
    Author: StevenDrain <stevendrain@u.boisestate.edu>
    Date:   Mon Mar 9 18:27:14 2026 -0600
    
        Added two more levers
    
    commit ddac6c55bda5fd4bf71f5ef2c034dec1925c3ea4
    Author: StevenDrain <stevendrain@u.boisestate.edu>
    Date:   Wed Feb 25 19:50:37 2026 -0700
    
        Forklift Lever
    
        -made lever logic
        -started work on the fork lifting part

---
+++ ForkliftWarriors/Assets/Scripts/RaiseFork.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+public class RaiseFork : MonoBehaviour
+{
+    [Header("Levers")]
+    [SerializeField] XRLever raiseLever;
+    [SerializeField] XRLever leftRightLever;
+    [SerializeField] XRLever tiltLever;
+
+    [Header("Fork Reference")]
+    [SerializeField] Transform forkTransform;
+
+    [Header("Speeds")]
+    [SerializeField] private float forkRaiseSpeed = 1f;
+    [SerializeField] private float forkSlideSpeed = 1f;
+    [SerializeField] private float forkTiltSpeed = 45f;
+
+    [Header("Limits")]
+    [SerializeField] private float minY, maxY;
+    [SerializeField] private float minZ, maxZ;
+    [SerializeField] private float minTilt, maxTilt;
+
+    private Vector3 restPosition;
+    private Quaternion restRotation;
+    private float currentTilt = 0f;
+    private const float deadzone = 0.1f;
+
+    void Start()
+    {
+        restPosition = forkTransform.localPosition;
+        restRotation = forkTransform.localRotation;
+    }
+
+    void Update()
+    {
+        RaisingFork();
+        LeftRightFork();
+        TiltFork();
+    }
+
+    // removes the small deadzone around centre so fork doesnt drift when lever is at rest
+    float ApplyDeadzone(float input)
+    {
+        if (Mathf.Abs(input) < deadzone) return 0f;
+        return Mathf.Sign(input) * (Mathf.Abs(input) - deadzone) / (1f - deadzone);
+    }
+
+    void RaisingFork()
+    {
+        float input = ApplyDeadzone(raiseLever.value);
+        Vector3 pos = forkTransform.localPosition;
+        pos.y += input * forkRaiseSpeed * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, restPosition.y + minY, restPosition.y + maxY);
+        forkTransform.localPosition = pos;
+    }
+
+    void LeftRightFork()
+    {
+        float input = ApplyDeadzone(leftRightLever.value);
+        Vector3 pos = forkTransform.localPosition;
+        pos.x += input * forkSlideSpeed * Time.deltaTime;
+        pos.x = Mathf.Clamp(pos.x, restPosition.x + minZ, restPosition.x + maxZ);
+        forkTransform.localPosition = pos;
+    }
+
+    void TiltFork()
+    {
+        float input = ApplyDeadzone(tiltLever.value);
+        currentTilt += input * forkTiltSpeed * Time.deltaTime;
+        currentTilt = Mathf.Clamp(currentTilt, minTilt, maxTilt);
+        forkTransform.localRotation = restRotation * Quaternion.Euler(currentTilt, 0f, 0f);
+    }
+}
